cba87238443fb958aa020c95e7192895cf775a41
ParkAsher <devasherpark@naver.com>
2023-08-17 20:52:28 +0900

    JavaScript var, let, const

---
+++ var,let,const/const.js
@@ -0,0 +1,8 @@
+const greeting = 'hello';
+console.log(greeting);
+
+// const greeting = 'hi';
+// console.log(greeting);
+
+greeting = 'how are you?';
+console.log(greeting);
---
+++ var,let,const/let.js
@@ -0,0 +1,8 @@
+let greeting = 'hello';
+console.log(greeting);
+
+// let greeting = 'hi';
+// console.log(greeting);
+
+greeting = 'how are you?';
+console.log(greeting);
