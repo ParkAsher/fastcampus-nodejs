c3ea81c5f2c50e19766422ecb57830e397f120de
ParkAsher <devasherpark@naver.com>
2024-06-13 13:12:03 +0900

    [feat]: confgiModule 적용하여 환경설정 적용

--- simple-board/src/app.controller.ts
+++ simple-board/src/app.controller.ts
@@ -7,10 +7,14 @@ import {
 } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Ip } from './decorators/ip.decorator';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-    constructor(private readonly appService: AppService) {}
+    constructor(
+        private readonly appService: AppService,
+        private readonly configService: ConfigService,
+    ) {}
 
     private readonly logger = new Logger(AppController.name);
 
@@ -19,6 +23,8 @@ export class AppController {
         // this.logger.log(ip);
         // this.logger.debug(ip);
         // this.logger.error(ip);
+
+        console.log(this.configService.get<string>('ENVIRONMENT'));
 
         // console.log(ip);
         return this.appService.getHello();
--- simple-board/src/app.module.ts
+++ simple-board/src/app.module.ts
@@ -1,12 +1,12 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 import { BoardModule } from './board/board.module';
 import { LoggingMiddleware } from './middlewares/logging.middleware';
+import configModule from './config';
 
 @Module({
-    imports: [BoardModule],
+    imports: [configModule(), BoardModule],
     controllers: [AppController],
     providers: [AppService],
 })
